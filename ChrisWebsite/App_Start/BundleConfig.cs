bf2658c48db475f7b26d2fac3f540f8eb4bc9cb9
Christian Wandsø <chri85w9@edu.sde.dk>
2020-06-21 16:51:53 +0200

    css

--- ChrisWebsite/App_Start/BundleConfig.cs
+++ ChrisWebsite/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ChrisWebsite
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-minty.css",
                       "~/Content/site.css"));
         }
     }
